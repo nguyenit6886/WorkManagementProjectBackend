411485fbd9a360457ddb9e397fd53dfa845253cc
nguyenit6886 <nguyenit6886@gmail.com>
2022-07-05 08:57:28 +0700

    remove result evaluation module

--- src/main/java/huce/edu/workmanagementprojectbackend/repository/ResultEvaluationReponsitory.java
+++
@@ -1,9 +0,0 @@
-package huce.edu.workmanagementprojectbackend.repository;
-
-import huce.edu.workmanagementprojectbackend.model.ResultEvaluationEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ResultEvaluationReponsitory extends JpaRepository<ResultEvaluationEntity, Integer> {
-}
