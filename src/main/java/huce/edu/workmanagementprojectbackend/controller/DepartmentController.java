d8949d2bdce880a3bd43f44a01ee2917e985a98f
nguyenit6886 <nguyenit6886@gmail.com>
2022-07-08 14:57:03 +0700

    demo validate

--- src/main/java/huce/edu/workmanagementprojectbackend/controller/DepartmentController.java
+++ src/main/java/huce/edu/workmanagementprojectbackend/controller/DepartmentController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class DepartmentController {
@@ -26,7 +28,15 @@ public class DepartmentController {
   }
 
   @RequestMapping("/save_department")
-  public String addDepartment(@ModelAttribute("department")DepartmentEntity department) {
+  public String addDepartment(@ModelAttribute("department")DepartmentEntity department,
+                              Model model) {
+    Map<String,String> errors = new HashMap<>();
+    errors.put("Name","Tên đã tồn tại");
+    if(errors.keySet().size() > 0){
+      model.addAttribute("error",errors);
+      model.addAttribute("department",department);
+      return "/html/Manager/department/manager-department";
+    }
     if(department.getId() != 0){
       department.setUpdateDate(new Date());
       iDepartmentService.updateObject(department);
